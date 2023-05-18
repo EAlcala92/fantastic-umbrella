69524be6318639830bf9872ae724a7c2b09af4bb
Tornado <esteban.j.alcala@gmail.com>
2023-05-17 21:22:48 -0700

    updates 1.1

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belogsTo(Category, {
+  foreignKey: 'catgory_id',
+});
 
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  foreignKey: 'category_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
